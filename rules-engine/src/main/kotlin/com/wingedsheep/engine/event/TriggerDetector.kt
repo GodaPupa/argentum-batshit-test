ee394357b351243010723b05b490e10d9783334d
GodaPupa <ericparanada@yahoo.com>
2026-09-08 19:10:46 -0700

    Match token predicates in sacrifice triggers

--- rules-engine/src/main/kotlin/com/wingedsheep/engine/event/TriggerDetector.kt
+++ rules-engine/src/main/kotlin/com/wingedsheep/engine/event/TriggerDetector.kt
@@ -2433,6 +2433,10 @@ class TriggerDetector(
                     !cardComponent.typeLine.isArtifact
                 is com.wingedsheep.sdk.scripting.predicates.CardPredicate.HasSubtype ->
                     cardComponent.typeLine.hasSubtype(predicate.subtype)
+                is com.wingedsheep.sdk.scripting.predicates.CardPredicate.IsToken ->
+                    entity.has<TokenComponent>()
+                is com.wingedsheep.sdk.scripting.predicates.CardPredicate.IsNontoken ->
+                    !entity.has<TokenComponent>()
                 else -> true
             }
         }
