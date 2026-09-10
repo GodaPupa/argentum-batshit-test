45b2bf3e04228bfb5e8ce55e2a72a6a695771113
GodaPupa <ericparanada@yahoo.com>
2026-09-09 20:00:31 -0700

    Add Project X lethal-loop diagnostics

--- mtg-sets/2024/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/ProjectXComboScenarioTest.kt
+++ mtg-sets/2024/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/ProjectXComboScenarioTest.kt
@@ -13,6 +13,7 @@ import com.wingedsheep.sdk.core.Step
 import com.wingedsheep.sdk.model.Deck
 import com.wingedsheep.sdk.model.EntityId
 import com.wingedsheep.sdk.scripting.AdditionalCostPayment
+import io.kotest.assertions.withClue
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
@@ -83,7 +84,14 @@ class ProjectXComboScenarioTest : FunSpec({
         var elite = d.putCreatureOnBattlefield(player, "Safehold Elite")
 
         repeat(3) {
-            d.sacrificeToFeeder(player, feeder, elite).isSuccess shouldBe true
+            val sacrifice = d.sacrificeToFeeder(player, feeder, elite)
+            withClue(
+                "iteration=$it error=${sacrifice.error} playerLife=${d.getLifeTotal(player)} " +
+                    "opponentLife=${d.getLifeTotal(opponent)} stack=${d.stackSize} " +
+                    "pending=${d.pendingDecision?.javaClass?.simpleName}",
+            ) {
+                sacrifice.isSuccess shouldBe true
+            }
             d.finishCycle(player, opponent)
             if (it < 2) elite = d.findPermanent(player, "Safehold Elite")!!
         }
