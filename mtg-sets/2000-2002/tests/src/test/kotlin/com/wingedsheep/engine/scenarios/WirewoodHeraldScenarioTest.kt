c97dec571a29b54ffcf524be7c9bd6fc9937e2fd
GodaPupa <ericparanada@yahoo.com>
2026-09-10 09:24:56 -0700

    Fix nonbattlefield subtype matching

--- mtg-sets/2000-2002/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/WirewoodHeraldScenarioTest.kt
+++ mtg-sets/2000-2002/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/WirewoodHeraldScenarioTest.kt
@@ -7,7 +7,7 @@ import io.kotest.matchers.shouldBe
 
 class WirewoodHeraldScenarioTest : ScenarioTestBase() {
     init {
-        listOf("Ivy Lane Denizen", "Evolution Witness", "Nettle Sentinel",
+        listOf("Safehold Elite", "Ivy Lane Denizen", "Evolution Witness", "Nettle Sentinel",
             "Birchlore Rangers", "Essence Warden", "Masked Vandal", "Quirion Ranger").forEach { role ->
             test("dying may tutor the Project X Elf role $role") {
                 val game = scenario().withPlayers("Player1", "Player2")
