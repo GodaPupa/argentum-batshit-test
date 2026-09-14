567a0916e2cfd8c356998e0c48e5d3aecaf42fdb
GodaPupa <ericparanada@yahoo.com>
2026-09-14 16:41:17 -0700

    fix(lilysplash): forward lilysplashLandChallenger to the test JVM

--- buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
+++ buildSrc/src/main/kotlin/kotlin-jvm.gradle.kts
@@ -62,7 +62,7 @@ tasks.withType<Test>().configureEach {
         "eclCollect", "eclCollectGames", "eclCollectSeed", "eclCollectOutput", "eclCollectBaseDir",
         "eclCollectRunId", "eclCollectStartIndex",
         "arenaGames", "arenaSeed", "arenaSet", "arenaMaxTurns", "arenaThreads",
-        "lilysplashPreflight",
+        "lilysplashPreflight", "lilysplashLandChallenger",
     )) {
         System.getProperty(prop)?.let { systemProperty(prop, it) }
     }
