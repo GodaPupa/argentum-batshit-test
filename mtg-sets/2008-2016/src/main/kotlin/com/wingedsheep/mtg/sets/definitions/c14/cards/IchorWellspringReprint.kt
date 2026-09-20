973ff874c59f94c44559e53d4c11460fe810fc9f
GodaPupa <ericparanada@yahoo.com>
2026-09-20 12:36:37 -0700

    Add Ichor Wellspring Commander reprints

---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c14/cards/IchorWellspringReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.c14.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Ichor Wellspring reprint in C14. Canonical CardDefinition lives in Mirrodin Besieged.
+ */
+val IchorWellspringReprint = Printing(
+    oracleId = "5b5ef43b-13fd-4461-8d2d-18be65e9a790",
+    name = "Ichor Wellspring",
+    setCode = "C14",
+    collectorNumber = "241",
+    scryfallId = "eab0d19f-1df7-48a9-bd39-3d8c26ae7bb6",
+    artist = "Steven Belledin",
+    imageUri = "https://cards.scryfall.io/normal/front/e/a/eab0d19f-1df7-48a9-bd39-3d8c26ae7bb6.jpg",
+    releaseDate = "2014-11-07",
+    rarity = Rarity.COMMON,
+)
---
+++ mtg-sets/2008-2016/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c16/cards/IchorWellspringReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.c16.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Ichor Wellspring reprint in C16. Canonical CardDefinition lives in Mirrodin Besieged.
+ */
+val IchorWellspringReprint = Printing(
+    oracleId = "5b5ef43b-13fd-4461-8d2d-18be65e9a790",
+    name = "Ichor Wellspring",
+    setCode = "C16",
+    collectorNumber = "258",
+    scryfallId = "94de25aa-d29a-45c9-8c4d-1ac27eb6a914",
+    artist = "Steven Belledin",
+    imageUri = "https://cards.scryfall.io/normal/front/9/4/94de25aa-d29a-45c9-8c4d-1ac27eb6a914.jpg",
+    releaseDate = "2016-11-11",
+    rarity = Rarity.COMMON,
+)
