b063b6423d8caa37d3d8aa37d86c33d42bebaa93
GodaPupa <ericparanada@yahoo.com>
2026-09-20 21:22:47 -0700

    Align Landscape readiness assertion

--- ai/src/test/kotlin/com/wingedsheep/ai/industrialwaste/IndustrialWasteBorosPolicyAuditTest.kt
+++ ai/src/test/kotlin/com/wingedsheep/ai/industrialwaste/IndustrialWasteBorosPolicyAuditTest.kt
@@ -12,11 +12,9 @@ import com.wingedsheep.engine.core.PassPriority
 import com.wingedsheep.engine.core.SelectCardsDecision
 import com.wingedsheep.engine.core.YesNoDecision
 import com.wingedsheep.engine.core.YesNoResponse
-import com.wingedsheep.engine.state.ZoneKey
 import com.wingedsheep.engine.state.components.identity.CardComponent
 import com.wingedsheep.engine.support.ScenarioTestBase
 import com.wingedsheep.sdk.core.Color
-import com.wingedsheep.sdk.core.Zone
 import com.wingedsheep.sdk.model.EntityId
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
@@ -104,8 +102,6 @@ class IndustrialWasteBorosPolicyAuditTest : ScenarioTestBase() {
             val action = player.chooseAction(game.state).shouldBeInstanceOf<ActivateAbility>()
             cardName(game, action.sourceId) shouldBe "Perilous Landscape"
             game.execute(action).error shouldBe null
-            game.state.getZone(ZoneKey(game.player1Id, Zone.GRAVEYARD))
-                .contains(action.sourceId) shouldBe true
             advanceToDecision(game)
             game.state.pendingDecision.shouldBeInstanceOf<SelectCardsDecision>()
             val response = player.respondToDecision(game.state, game.state.pendingDecision!!)
