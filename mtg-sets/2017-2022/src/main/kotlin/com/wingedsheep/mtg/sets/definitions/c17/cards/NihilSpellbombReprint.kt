528af5e2becaca4a739dd8f43e5d34de46af9b02
GodaPupa <ericparanada@yahoo.com>
2026-09-21 06:45:12 -0700

    Add C17 Nihil Spellbomb printing

---
+++ mtg-sets/2017-2022/src/main/kotlin/com/wingedsheep/mtg/sets/definitions/c17/cards/NihilSpellbombReprint.kt
@@ -0,0 +1,19 @@
+package com.wingedsheep.mtg.sets.definitions.c17.cards
+
+import com.wingedsheep.sdk.model.Printing
+import com.wingedsheep.sdk.model.Rarity
+
+/**
+ * Nihil Spellbomb reprint in C17. Canonical CardDefinition lives in Scars of Mirrodin.
+ */
+val NihilSpellbombReprint = Printing(
+    oracleId = "b713e49f-1b13-42d1-91f2-cc7a579e7614",
+    name = "Nihil Spellbomb",
+    setCode = "C17",
+    collectorNumber = "218",
+    scryfallId = "7012494f-0b9b-4634-b608-328649bec97c",
+    artist = "Franz Vohwinkel",
+    imageUri = "https://cards.scryfall.io/normal/front/7/0/7012494f-0b9b-4634-b608-328649bec97c.jpg",
+    releaseDate = "2017-08-25",
+    rarity = Rarity.COMMON,
+)
