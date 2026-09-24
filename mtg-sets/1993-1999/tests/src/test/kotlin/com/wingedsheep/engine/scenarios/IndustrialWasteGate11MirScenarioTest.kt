efa15ea88c6a779ea09e5a72b3911d46033f6be3
GodaPupa <ericparanada@yahoo.com>
2026-09-24 05:18:12 -0700

    Gate 11: qualify Wall of Roots

---
+++ mtg-sets/1993-1999/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/IndustrialWasteGate11MirScenarioTest.kt
@@ -0,0 +1,36 @@
+package com.wingedsheep.engine.scenarios
+
+import com.wingedsheep.engine.core.ActivateAbility
+import com.wingedsheep.engine.state.components.battlefield.CountersComponent
+import com.wingedsheep.engine.state.components.player.ManaPoolComponent
+import com.wingedsheep.engine.support.GameTestDriver
+import com.wingedsheep.engine.support.TestCards
+import com.wingedsheep.mtg.sets.definitions.mir.cards.WallOfRoots
+import com.wingedsheep.sdk.core.Step
+import com.wingedsheep.sdk.model.Deck
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class IndustrialWasteGate11MirScenarioTest : FunSpec({
+    test("Wall of Roots adds green, pays a -0/-1 counter, and activates only once each turn") {
+        val d = GameTestDriver()
+        d.registerCards(TestCards.all)
+        d.registerCard(WallOfRoots)
+        d.initMirrorMatch(deck = Deck.of("Forest" to 30), startingLife = 20)
+        val p1 = d.activePlayer!!
+        d.passPriorityUntil(Step.PRECOMBAT_MAIN)
+
+        val wall = d.putCreatureOnBattlefield(p1, "Wall of Roots")
+        val abilityId = WallOfRoots.activatedAbilities.single().id
+        val action = ActivateAbility(playerId = p1, sourceId = wall, abilityId = abilityId)
+
+        d.submitSuccess(action)
+
+        d.state.getEntity(p1)!!.get<ManaPoolComponent>()!!.green shouldBe 1
+        d.state.getEntity(wall)!!.get<CountersComponent>()!!.counters.values.sum() shouldBe 1
+
+        d.submitExpectFailure(action)
+        d.state.getEntity(p1)!!.get<ManaPoolComponent>()!!.green shouldBe 1
+        d.state.getEntity(wall)!!.get<CountersComponent>()!!.counters.values.sum() shouldBe 1
+    }
+})
