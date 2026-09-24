a15dd7781eb199985dca91c73f07df2e18563c7f
GodaPupa <ericparanada@yahoo.com>
2026-09-24 08:53:26 -0700

    Gate 11: force Mesmeric Fiend choice in linked-exile scenario

--- mtg-sets/2000-2002/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/IndustrialWasteGate11TorScenarioTest.kt
+++ mtg-sets/2000-2002/tests/src/test/kotlin/com/wingedsheep/engine/scenarios/IndustrialWasteGate11TorScenarioTest.kt
@@ -25,7 +25,7 @@ class IndustrialWasteGate11TorScenarioTest : FunSpec({
         d.passPriorityUntil(Step.PRECOMBAT_MAIN)
 
         val victim = d.putCardInHand(p2, "Lightning Bolt")
-        val land = d.putCardInHand(p2, "Mountain")
+        val alternative = d.putCardInHand(p2, "Centaur Courser")\n        val land = d.putCardInHand(p2, "Mountain")
         val fiendCard = d.putCardInHand(p1, "Mesmeric Fiend")
         d.giveMana(p1, Color.BLACK, 2)
         d.castSpell(p1, fiendCard)
