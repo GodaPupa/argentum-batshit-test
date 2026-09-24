61d61c6fe10494ced582f123d7f4a3a6057f5eb4
GodaPupa <ericparanada@yahoo.com>
2026-09-24 05:28:16 -0700

    Commit Prototype BRO golden and diagnose legacy hash

--- ai/src/test/kotlin/com/wingedsheep/ai/arena/FrozenBaselineTest.kt
+++ ai/src/test/kotlin/com/wingedsheep/ai/arena/FrozenBaselineTest.kt
@@ -48,6 +48,11 @@ class FrozenBaselineTest : FunSpec({
             recordActionStream = true,
         )
 
+        println(
+            "FROZEN_BASELINE_DIAGNOSTIC hash=${outcome.actionStreamHash} turns=${outcome.turns} " +
+                "winner=${outcome.winnerSeat} life=${outcome.seat0Life}/${outcome.seat1Life}"
+        )
+
         withClue(
             "LEGACY_V0's behaviour moved. Actual hash: ${outcome.actionStreamHash} " +
                 "(${outcome.turns} turns, winner seat ${outcome.winnerSeat}, " +
