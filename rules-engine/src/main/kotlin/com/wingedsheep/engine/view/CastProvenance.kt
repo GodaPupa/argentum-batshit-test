7e3a9c4e7bc45324984108861b417baae297a5c1
GodaPupa <ericparanada@yahoo.com>
2026-09-25 15:39:29 -0700

    Record accepted Bestow cast provenance

--- rules-engine/src/main/kotlin/com/wingedsheep/engine/view/CastProvenance.kt
+++ rules-engine/src/main/kotlin/com/wingedsheep/engine/view/CastProvenance.kt
@@ -119,6 +119,7 @@ object CastProvenance {
         AlternativeCostType.WARP -> "warp"
         AlternativeCostType.DASH -> "dash"
         AlternativeCostType.EVOKE -> "evoke"
+        AlternativeCostType.BESTOW -> "bestow"
         AlternativeCostType.EMERGE -> "emerge"
         AlternativeCostType.SNEAK -> "sneak"
         AlternativeCostType.WEB_SLINGING -> "web-slinging"
